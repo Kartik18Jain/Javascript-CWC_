89da6a58eede1e681b04c862d6fa6b92adfe350d
Kartik18Jain <kj0302279@gmail.com>
2026-06-01 11:37:21 +0000

    conversion of datatypes

---
+++ Lecture_01/Cnversion_in_js.js
@@ -0,0 +1,30 @@
+let score = 33;
+console.log(score);
+
+let valueInNumber = Number("33");
+console.log(typeof(valueInNumber));
+
+let valueinString = String(33);
+console.log(typeof(valueinString));
+
+//Bollean 1=> true ; Boolean 0=> false
+let isLoggedIn = Boolean(1);
+console.log(isLoggedIn);
+
+let isLoggedIn2 = Boolean(0);
+console.log(isLoggedIn2);
+
+let someNumber = 3;
+let isLoggedIn3 = Boolean(someNumber);
+console.log(isLoggedIn3);
+
+let someNumber2 = 0;
+let isLoggedIn4 = Boolean(someNumber2);
+console.log(isLoggedIn4);
+
+let isLoggedIn5 = Boolean("Kartik");
+console.log(isLoggedIn5);
+
+let isLoggedIn6 = Boolean("");
+console.log(isLoggedIn6);
+
