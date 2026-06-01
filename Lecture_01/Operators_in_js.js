14ad606e7349a52f3c5bba45b88ccbeafef38f7e
Kartik18Jain <kj0302279@gmail.com>
2026-06-01 11:53:04 +0000

    Operators in js

---
+++ Lecture_01/Operators_in_js.js
@@ -0,0 +1,63 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(3+2); // => 5
+console.log(3-2); // => 1
+console.log(3*2); // => 6
+console.log(3/2); // => 1.5
+console.log(3%2); // => 1
+
+let str1 = "Hello";
+let str2 = "World";
+console.log(str1 + str2); // =>HelloWorld
+console.log(str1 + " " + str2); // => Hello World
+
+let str3 = "5";
+let str4 = "10";
+console.log(str3 + str4); // => 510
+
+let num1 = 5;
+let num2 = 10;
+console.log(num1 + num2); // => 15
+
+let mixed1 = "5";
+let mixed2 = 10;
+console.log(mixed1 + mixed2); // => 510
+
+let a = 5;
+a += 3; // a = a + 3
+console.log(a); // => 8
+
+let b = 10;
+b -= 4; // b = b - 4
+console.log(b); // => 6
+
+let c = 6;
+c *= 2; // c = c * 2
+console.log(c); // => 12
+
+let d = 20;
+d /= 4; // d = d / 4
+console.log(d); // => 5
+
+let e = 7;
+e %= 3; // e = e % 3
+console.log(e); // => 1
+
+let f = 5;
+f++; // f = f + 1
+console.log(f); // => 6
+
+let g = 10;
+g--; // g = g - 1
+console.log(g); // => 9
+
+let h = 5;
+console.log(++h); // => 6 (pre-increment)
+console.log(h);   // => 6
+
+let i = 10;
+console.log(i--); // => 10 (post-decrement)
+console.log(i);   // => 9
+
