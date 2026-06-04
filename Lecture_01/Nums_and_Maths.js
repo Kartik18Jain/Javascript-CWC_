bd15189877b3a7143a8972a18fa8cc4616063f2c
Kartik18Jain <kj0302279@gmail.com>
2026-06-04 17:06:56 +0000

    Nums and Maths in js

---
+++ Lecture_01/Nums_and_Maths.js
@@ -0,0 +1,49 @@
+/******************Numbers ************************ */
+
+//Integers
+let num = 10;
+console.log(num)// output : 10;
+
+//Floating-point numbers
+let floatNum = 3.14;
+console.log(floatNum); // output : 3.14
+
+//Negative numbers
+let negNum = -5;
+console.log(negNum); // output : -5
+
+//Exponential notation
+let expNum = 1.23e4; // equivalent to 1.23 * 10^4
+console.log(expNum); // output : 12300
+
+//NaN (Not a Number)
+let notANum = NaN;
+console.log(notANum); // output : NaN
+
+//Infinity
+let inf = Infinity;
+console.log(inf); // output : Infinity
+
+//Negative Infinity
+let negInf = -Infinity;
+console.log(negInf); // output : -Infinity
+ 
+/************************Maths********************* */
+
+//basic of maths
+console.log(3 + 2); // output : 5
+console.log(3 - 2); // output : 1
+console.log(3 * 2); // output : 6
+console.log(3 / 2); // output : 1.5
+console.log(3 % 2); // output : 1
+
+//Math object
+console.log(Math.PI); // output : 3.141592653589793
+console.log(Math.E); // output : 2.718281828459045
+console.log(Math.sqrt(16)); // output : 4 // square root of 16
+console.log(Math.pow(2, 3)); // output : 8 // 2 raised to the power of 3
+console.log(Math.abs(-5)); // output : 5 changes the sign of the number to positive for example -5 becomes 5
+console.log(Math.floor(3.7)); // output : 3 // rounds the number down to the nearest integer
+console.log(Math.ceil(3.2)); // output : 4 // rounds the number up to the nearest integer
+console.log(Math.round(3.5)); // output : 4 // rounds the number to the nearest integer
+
