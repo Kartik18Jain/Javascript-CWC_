10e7e759142b01cf29db24b9041d059426584d97
Kartik18Jain <kj0302279@gmail.com>
2026-07-23 15:59:52 +0000

    date amd time in js

---
+++ Lecture_01/07_Dateinjs.js
@@ -0,0 +1,8 @@
+const myDate = new Date()
+console.log(myDate.toDateString()) // output:- Wed Jul 22 2026
+console.log(myDate.toISOString()) //output:-  2026-07-22T11:12:18.647Z
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toLocaleTimeString())
+console.log(typeof myDate)
